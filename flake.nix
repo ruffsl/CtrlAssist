4fdebc5bfcb784b129c91512907c624540898f8f
ruffsl <roxfoxpox@gmail.com>
2026-01-02 18:05:11 -0600

    feat: add dependency uhid-virt
    - https://github.com/Oxen-AI/Oxen/blob/63e4976770831ea0d48fc48ad2ea55c3606142d5/oxen-rust/flake.nix#L45

--- flake.nix
+++ flake.nix
@@ -28,23 +28,26 @@
         devShell = pkgs.mkShell {
           packages = with pkgs; [
             appstream
-            # clang
+            clang
             flatpak-builder
             # gcc
             jstest-gtk
             librsvg
             linuxConsoleTools
             lldb
+            llvmPackages.libclang
+            llvmPackages.llvm
             pkg-config
             python313Packages.aiohttp
             python313Packages.tomlkit
             rust
             udev
           ];
-          # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load
-          # SVG files (important for icons)
-          # Fixes error: .../share/icons/hicolor/scalable/apps/blabla.svg is not a valid icon: Format not recognized
-          GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
+           # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load
+           # SVG files (important for icons)
+           # Fixes error: .../share/icons/hicolor/scalable/apps/blabla.svg is not a valid icon: Format not recognized
+           GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages.libclang.lib];
         };
       }
     );
